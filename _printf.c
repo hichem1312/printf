2a520ce97fc445a100a38fac418c87e5ac2a8b88
hichem <hichemmaatoug55@gmail.com>
2022-03-14 12:49:19 -0700

    c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,61 @@
 #include "main.h"
+#include <string.h>
+/**
+ * print_number - writes the character c to stdout
+ * @n:integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_number(int n)
+{int x, k, r = 1, s = 0;
 
+	if (n == 0)
+	{
+		putchar(48);
+		s += 1;
+		return (s);
+	}
+	x = n;
+	if (x == -2147483648)
+	{
+		x = x + 1;
+	}
+	if (n < 0)
+	{
+		x = -x;
+		putchar('-');
+		s += 1;
+	}
+	while (r < 1000000000 && (x - (r * 10) >= 0))
+	{
+		r = r * 10;
+	}
+	while (x > 0)
+	{
+		k = x / r;
+		x = x % r;
+		r /= 10;
+		if (x < r)
+		{
+			putchar(k + 48);
+			 s += 1;
+			putchar(48);
+			 s += 1;
+			r /= 10;
+		}
+		else if (n == -2147483648 && k == 7 && r == 0)
+		{
+			putchar(k + 49);
+			 s += 1;
+		}
+		else
+		{
+			putchar(k + 48);
+			 s += 1;
+		}
+	}
+	return (s);
+}
 /**
  *pr_char - pr
  *@l: lx
@@ -47,22 +103,22 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			if (format[i] == 'd')
+			if (format[i + 1] == 'd')
 			{
 				j = va_arg(l, int);
 				length += print_number(j);
 			}
-			if (format[i] == 'c')
+			if (format[i + 1] == 'c')
 			{
 				c = va_arg(l, int);
 				length += pr_char(c);
 			}
-			if (format[i] == 's')
+			if (format[i + 1] == 's')
 			{
 				ch = va_arg(l, char*);
 				length += pr_string(ch);
 			}
-			if (format[i] == '%')
+			if (format[i + 1] == '%')
 			{
 				putchar('%');
 				length += 1;
