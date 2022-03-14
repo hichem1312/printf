6e5e2aa6f89ecc640b0129842c301968be3928b3
hichem <hichemmaatoug55@gmail.com>
2022-03-14 11:49:24 -0700

    s

--- main.h
+++ main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
-
+#include <unistd.h>
 #include <stdlib.h>
 
 int _printf(const char *format, ...);
+int pr_char(char c);
+int pr_string(char *ch);
+int print_number(int n);
+int _putchar(char c);
+
 #endif
